32b9cbce4082aa4dbc3230a088c5aa9452e67860
iqiyi <lzz804456852->
2019-05-22 01:00:54 +0800

    修改main.py文件

--- cygSpider/main.py
+++ cygSpider/main.py
@@ -1,13 +1,10 @@
 from scrapy.cmdline import execute
 import os
 import sqlite3
-import sys
-# print(os.path.abspath(os.path.dirname(__file__)))
-# execute("scrapy crawl roleinfo".split())
+
+execute("scrapy crawl roleinfo".split())
 
 conn =sqlite3.connect("../db.sqlite3", isolation_level=None)
-#conn.execute("delete from role_role where role_role.rowid not in (select MAX(role_role.rowid) from role_role group by detail_url)")
-#conn.execute('vacuum')
-cursor = conn.execute("SELECT * from role_role Where menpai='少林'")
-for i in cursor:
-    print(i)
+conn.execute("delete from role_role where role_role.rowid not in (select MAX(role_role.rowid) from role_role group by detail_url)")
+conn.execute('vacuum')
+conn.close()
