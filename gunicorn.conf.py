0d9dfbe71e5a5558c091f58e6e573c4ef1edc1cd
lzz <1500202278@qq.com>
2019-11-01 20:04:44 +0800

    修改gunicorn监控端口

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,9 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"   #绑定的ip与端口
-workers = 2                #核心数
-errorlog = './gunicorn/gunicorn.error.log' #发生错误时log的路径
-accesslog = './gunicorn/gunicorn.access.log' #正常时的log路径
+bind = "0.0.0.0:8082"   #绑定的ip与端口
+workers = 3                #核心数
+errorlog = '/home/cyg/gunicorn/gunicorn.error.log' #发生错误时log的路径
+accesslog = '/home/cyg/gunicorn/gunicorn.access.log' #正常时的log路径
 #loglevel = 'debug'   #日志等级
 proc_name = 'gunicorn_cygsite'   #进程名
 
